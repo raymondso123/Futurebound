bd84bc6c5c553a70bc3752620ef378b693920dcc
raymondso123 <167659047+raymondso123@users.noreply.github.com>
2024-06-13 19:05:18 -0400

    Title Screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -3,10 +3,17 @@ import greenfoot.*;
 public class TitleScreen extends World {
 
     public TitleScreen() {
-        super(800, 600, 1); // Adjust size as needed
+        super(600, 400, 1);
         prepare();
     }
-
+    
+    public void act() {
+        if (Greenfoot.isKeyDown("space")) {
+            Greenfoot.setWorld(new MyWorld());
+            MyWorld.started = true;
+        }
+    }
+    
     private void prepare() {
         // Add title image or text
         GreenfootImage titleImage = new GreenfootImage("title.png");
